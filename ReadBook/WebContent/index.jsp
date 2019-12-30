260319f6335dfc5abf2872296871a76f99a697b3
zhujunliang <1127766234@qq.com>
2019-12-30 14:33:51 +0800

    提交

---
+++ ReadBook/WebContent/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF8"
+    pageEncoding="UTF8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF8">
+<title>借书吧平台</title>
+</head>
+<body>
+登录
+</body>
+</html>
--- ReadBook/src/com/readbook/controller/UserAddServlet.java
+++ ReadBook/src/com/readbook/controller/UserAddServlet.java
@@ -17,7 +17,7 @@ public class UserAddServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("============");
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
