e46866a853b9b346df5e7a689bc7abb52054a7e2
zhujunliang <1127766234@qq.com>
2019-12-31 12:10:25 +0800

    修改filter

--- ReadBook/src/com/readbook/filters/ServletFilter.java
+++ ReadBook/src/com/readbook/filters/ServletFilter.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet¹ıÂËÆ÷
  */
-@WebFilter("/ServletFilter")
+@WebFilter(urlPatterns="*")
 public class ServletFilter implements Filter {
 
 	public void destroy() {
